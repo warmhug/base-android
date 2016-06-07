db3ce9c9a1006f2d937dfc1c6f7f5c09c8f3b8e3
然则 <hualei.hl@alibaba-inc.com>
2016-06-07 17:20:42 +0800

    first commit

---
+++ app/src/main/java/com/base/hua/baseandroid/MainActivity.java
@@ -0,0 +1,40 @@
+package com.base.hua.baseandroid;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn;
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
+
+        txt = (TextView) findViewById(R.id.textView);
+
+        ImageButton fab = (ImageButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button) {
+            txt.setText("clicked button");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+
+## Android
+
+使用 Android 最新支持的 ConstraintLayout 布局。
+
+### 注意点:
+app/build.gradle 里的`targetSdkVersion`要和预览的手机设备 skdVersion 一样。
+
