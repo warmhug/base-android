570cf7390403d2ac4f8af0b3b0f9b4cca1c01367
然则 <hualei.hl@alibaba-inc.com>
2017-02-06 18:21:40 +0800

    add webview & debugger support

--- app/src/main/java/com/base/hua/baseandroid/DisplayMessageActivity.java
+++ app/src/main/java/com/base/hua/baseandroid/DisplayMessageActivity.java
@@ -1,18 +1,26 @@
 package com.base.hua.baseandroid;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+public class DisplayMessageActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn;
+    boolean isDebugger = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-        
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = new TextView(this);
@@ -21,5 +29,41 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.activity_display_message);
         layout.addView(textView);
+
+        // 渲染 webview
+        WebView webView = (WebView) findViewById(R.id.wb);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setUseWideViewPort(true);
+        // webView.getSettings().setBuiltInZoomControls(true);
+        // webView.getSettings().setDisplayZoomControls(false);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("http://baidu.com");
+
+        // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+        //     WebView.setWebContentsDebuggingEnabled(true);
+        // }
+
+        btn = (Button) findViewById(R.id.button2);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button2) {
+            if (isDebugger) {
+                isDebugger = false;
+                btn.setText("debugger wb");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    WebView.setWebContentsDebuggingEnabled(false);
+                }
+            } else {
+                isDebugger = true;
+                btn.setText("stop debugger wb");
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    WebView.setWebContentsDebuggingEnabled(true);
+                }
+            }
+        }
     }
 }
